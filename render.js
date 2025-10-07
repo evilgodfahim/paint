57da9d08640e5a7d2cfa30a7d6461a5f7abf3ce7
Fahim <49230752+evilgodfahim@users.noreply.github.com>
2025-10-07 23:59:05 +0600

    Update render.js

--- render.js
+++ render.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 
 (async () => {
-  const url = "https://www.prothomalo.com/opinion"; 
+  const url = "https://en.prothomalo.com/opinion"; 
   const outputFile = path.resolve(__dirname, "opinion.html"); // save directly in root
 
   const browser = await puppeteer.launch({ headless: true, args: ["--no-sandbox"] });
